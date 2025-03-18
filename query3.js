834b2702910b6376063d72829c7a0f8db6848d68
Paul Vander Woude <pavander@umich.edu>
2025-03-18 15:53:47 -0400

    query3

--- query3.js
+++ query3.js
@@ -9,7 +9,15 @@
 function cities_table(dbname) {
     db = db.getSiblingDB(dbname);
 
-    // TODO: implement cities collection here
+    db.users.aggregate([
+        {
+            $group: {
+                _id: "current.city",
+                users: { $push: "$user_id" }
+            }
+        },
+        { $out: "cities" }
+    ]);
 
     return;
 }
